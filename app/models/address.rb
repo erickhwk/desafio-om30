666177891ee85b818db8a55830b1141d43e522de
Erick Valente <erickhwk@gmail.com>
2023-03-16 23:33:10 -0300

    refactor: move states_array from formview to a constant in address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -5,4 +5,7 @@ class Address < ApplicationRecord
 
   validates :zipcode, :street, :district, :city, :state, presence: true
   validates_correios_cep_of :zipcode
+  validate :validate_state
+
+  STATES_ARRAY = %w[AC AL AP AM BA CE DF ES GO MA MS MT MG PA PB PR PE PI RJ RN RS RO RR SC SP SE TO].freeze
 end
